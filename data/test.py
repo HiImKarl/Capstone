82c3a593f26979ee49f142ed036d357f0a9e0990
karl chen <karl.chen@mail.utoronto.ca>
2018-12-03 11:07:18 -0500

    adding stuff like ligma

--- data/test.py
+++ data/test.py
@@ -1,11 +1,14 @@
-import requests
+import csv
 
-params = {
-        'action': 'history',
-        'symbol': 'MSFT',
-        'interval': 'daily',
-        'period': 10
-        }
+l = []
+with open('farma_new.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        l.append([row[0], float(row[1]) / 100, float(row[2]) / 100, float(row[3]) / 100, float(row[4]) / 100])
 
-print(requests.get(url='http://api.kibot.com/', params=params).content.decode('utf-8'))
+
+with open('improved.csv', 'w') as f:
+    writer = csv.writer(f)
+    for row in l:
+        writer.writerow(row)
 
